54b29c1d85ed801d76a24a6a802c889aa8cd4abc
Giulia <giuliac35@gmail.com>
2023-02-02 18:06:17 +0100

    Create individualMarksListLisa List in main method and add values

--- src/it/develhope/genericWildcards/Start.java
+++ src/it/develhope/genericWildcards/Start.java
@@ -1,7 +1,13 @@
 package it.develhope.genericWildcards;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Start {
     public static void main(String[] args) {
+        List<Double> individualMarksListLisa = new ArrayList<>();
+        individualMarksListLisa.add(8.9);
+        individualMarksListLisa.add(7.5);
 
     }
 }
